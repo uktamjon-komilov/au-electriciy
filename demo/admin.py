757a6dc3998fa34d1e5f75b014d657d2673f2772
Uktamjon Komilov <uktamjon_komilov@epam.com>
2022-10-27 18:50:52 +0500

    django admin settings

--- demo/admin.py
+++ demo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from demo.models import Record
 
-# Register your models here.
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ["nmi", "meter_serial_number", "read", "date", "file_name"]
+    list_display_links = ["nmi", "meter_serial_number"]
+    search_fields = ["nmi", "meter_serial_number"]
+
+
+admin.site.register(Record, RecordAdmin)
