12d26bdac31d7101260805f53622f10cc30f9dd1
Uktamjon Komilov <uktamjon_komilov@epam.com>
2022-10-27 18:20:52 +0500

    added helper functions in utils.py and tested

--- demo/tests/test_utils.py
+++ demo/tests/test_utils.py
@@ -1,9 +1,37 @@
 import pytest
-from demo.utils import parse_datetime
+from demo.utils import parse_datetime, extract_values, create_record_from_values
 from django.utils import timezone
+import os
+from django.conf import settings
 
 
 def test_parse_datetime_works():
     assert parse_datetime("20040107100333") == timezone.datetime(2004, 1, 7, 10, 3, 33)
     assert parse_datetime("20040103094843") == timezone.datetime(2004, 1, 3, 9, 48, 43)
     assert parse_datetime("20040104141941") == timezone.datetime(2004, 1, 4, 14, 19, 41)
+
+
+def test_extract_values_works():
+    path: str = os.path.join(settings.BASE_DIR, "demo", "tests", "fixtures", "test.csv")
+    data: List[dict] = extract_values(path)
+
+    row = data[0]
+
+    assert row["nmi"] == "7142747824"
+    assert row["meter_serial_number"] == "DYRGBH6BWPT5"
+    assert row["read"] == "99772.0"
+    assert row["date"] == "20040103094843"
+
+
+@pytest.mark.django_db
+def test_create_record_from_values():
+    path: str = os.path.join(settings.BASE_DIR, "demo", "tests", "fixtures", "test.csv")
+    data: List[dict] = extract_values(path)
+    record = create_record_from_values(data[0])
+
+    assert record.id < 999
+    assert record.nmi == "7142747824"
+    assert record.meter_serial_number == "DYRGBH6BWPT5"
+    assert record.read == "99772.0"
+    assert record.date == parse_datetime("20040103094843")
+    assert record.file_name == "test.csv"
--- demo/utils.py
+++ demo/utils.py
@@ -1,5 +1,35 @@
 from django.utils.timezone import datetime
+from typing import List
+from django.core.management.base import CommandError
+import os
+from demo.models import Record
 
 
 def parse_datetime(value: str) -> datetime:
     return datetime.strptime(value, "%Y%m%d%H%M%S")
+
+
+def extract_values(path: str):
+    data: List[dict] = []
+    with open(path, "r") as file:
+        file.readline()
+        for index, line in enumerate(file.readlines()):
+            parts = line.split(",")
+            try:
+                data.append({
+                    "nmi": parts[1],                    
+                    "meter_serial_number": parts[6],                    
+                    "read": parts[13],                    
+                    "date": parts[14],
+                    "file_name": os.path.basename(path)   
+                })
+            except IndexError:
+                raise CommandError("There is error while extracting data from CSV (line {})".format(index+1))
+    return data
+
+
+def create_record_from_values(data) -> Record:
+    data["date"] = parse_datetime(data["date"])
+    record = Record(**data)
+    record.save()
+    return record
